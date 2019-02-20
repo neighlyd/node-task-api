84a6a30133d47548234980e8097058f0f901e2d7
Dustin Neighly <neighlyd@gmail.com>
2019-02-20 12:56:12 -0800

    Added GET route

--- server/server.js
+++ server/server.js
@@ -21,7 +21,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
-// Challenge - set text for a todo.
+app.get('/todos', (req, res) => {
+   Todo.find().then((todos) => {
+       // Send the todos back as an object, since that allows us to expand it in the future with additional properties.
+       res.send({todos});
+   }, (e) => {
+       res.status(400).send(e);
+   });
+});
 
 app.listen(3000, () => {
     console.log(`Started on port 3000`)
