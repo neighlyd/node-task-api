e991f6d33e2ec956d1e3e6bb626ebb59a22a9f91
Dustin Neighly <neighlyd@gmail.com>
2019-02-20 13:02:21 -0800

    Add test for GET /todos

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,9 +4,17 @@ const expect = require('expect');
 const {app} = require('./../server/server');
 const {Todo} = require('./../server/models/todo');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-    // The Udemy course uses Todos.remove({}), but this is deprecated.
-    Todo.deleteMany({}).then(() => done());
+    // The Udemy course uses db.remove({}), but this is deprecated; so we use db.deleteMany();
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -23,7 +31,7 @@ describe('POST /todos', () => {
                 if (err) {
                     return done(err);
                 }
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -41,9 +49,22 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
     });
 });
