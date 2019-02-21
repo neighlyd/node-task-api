7fb70f4bfe932d63f6936cc4f33ec690654b15be
Dustin Neighly <neighlyd@gmail.com>
2019-02-20 16:27:42 -0800

    Setup separate test and development ENVs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
+
+// Heroku sets process.env.NODE_ENV === 'production'
+// we can then set these to 'test' or 'development' to modify the MongoDB as desired.
