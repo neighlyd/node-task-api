e1d9fb6d1dd412f3b606d88d6c8cb1b63216949c
Dustin Neighly <neighlyd@gmail.com>
2019-03-22 15:42:10 -0700

    fixed user creation

--- src/routes/user.js
+++ src/routes/user.js
@@ -22,12 +22,15 @@ const avatarUpload = multer({
 })
 
 router.post('/users', async (req, res) => {
+    const user = await new User(req.body)
+    
     try {
         await user.save();
-        // sendWelcomeEmail(user.email, user.name)
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken();
         res.status(201).send({token, user})
     } catch(e) {
+        console.log(e)
         res.status(400).send(e);
     }
 });
